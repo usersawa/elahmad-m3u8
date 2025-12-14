d65cc082619c9ac7ffbdd60ef2314c20a819ba2f
usersawa <Youtabe388@gmail.com>
2025-12-14 19:29:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 const express = require("express");
-const fetch = require("node-fetch");
+const proxy = require("express-http-proxy");
 
 const app = express();
+const TARGET = "https://p-ltv.akamaized.net/ltv";
 
-// رابط البث الجديد
-const TARGET_URL = "https://p-ltv.akamaized.net/ltv/ltv.m3u8";
-
-app.get("*", async (req, res) => {
-  try {
-    const response = await fetch(TARGET_URL, {
-      headers: {
-        "User-Agent": "Mozilla/5.0 (Android TV; Android 13)",
-        "Accept": "*/*"
-      }
-    });
-
-    // Headers مهمة لتشغيل HLS
-    res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
-    // بث مباشر (streaming حقيقي)
-    response.body.pipe(res);
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Proxy error");
+app.use("/", proxy(TARGET, {
+  proxyReqPathResolver: req => `/ltv.m3u8`,
+  proxyReqOptDecorator: opts => {
+    opts.headers["User-Agent"] = "Mozilla/5.0 (Android TV; Android 13)";
+    return opts;
   }
-});
+}));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("HLS Proxy running");
-});
+app.listen(process.env.PORT || 3000, () => console.log("HLS Proxy running"));
